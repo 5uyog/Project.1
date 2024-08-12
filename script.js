cb833a19ddf718f55cd0384ba70099f3eee977bc
5uyog <shirpesuyog@gmail.com>
2024-08-12 21:46:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function myfunction() {
+function lightheme() {
     document.getElementById("theme").style.backgroundColor="white"
+    document.getElementById("light").style.display='none'
+    document.getElementById("dark").style.display='block'
     
+}
+function darktheme() {
+    document.getElementById("theme").style.backgroundColor="black"
+    document.getElementById("dark").style.display='none'
+    document.getElementById("light").style.display='block'
 }
    
