345877679860668135dd539dd865d43ffd729d87
5uyog <shirpesuyog@gmail.com>
2024-08-18 14:41:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function darktheme() {
     document.getElementById("light").style.display='block'
 }
    
+function btn0(){
+    document.getElementById("btn0").style.display='none'
+
+    document.getElementById("btn1").style.display='block'
+
+    document.getElementById("btn2").style.display='block'
+
+    document.getElementById("btn3").style.display='block'
+
+    document.getElementById("btn4").style.display='block'
+    
+    document.getElementById("btn5").style.display='block'
+}
+
+function btn1(){
+    document.getElementById("btn0").style.display='block'
+
+    document.getElementById("btn1").style.display='none'
+
+    document.getElementById("btn2").style.display='none'
+
+    document.getElementById("btn3").style.display='none'
+
+    document.getElementById("btn4").style.display='none'
+    
+    document.getElementById("btn5").style.display='none'
+}
+
+
